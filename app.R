d6122a9b71ddd26637521a4cbca8e33e398c54dc
Derek Wilson <derekw@derekw.org>
2019-03-25 12:33:25 +0000

    Added navbar with Plot and About tabPanels

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 
 library(shiny)
+library(markdown)
 library(data.table)
 
 cammet <- readRDS("data/CamMetCleanishData.RData")
@@ -7,50 +8,55 @@ cammet <- readRDS("data/CamMetCleanishData.RData")
 
 ui <- shinyUI(fluidPage(
 
-  titlePanel("Cambridge Weather"),
-
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Explore ", tags$a(href="https://www.cl.cam.ac.uk/research/dtg/weather/", "ComLab"),  "weather measurements:"),
-      selectInput("year", 
-                  strong("Choose year"), 
-                  choices = c("2019" = 2019,
-                              "2018" = 2018,
-                              "2017" = 2017,
-                              "2016" = 2016,
-                              "2015" = 2015,
-                              "2014" = 2014,
-                              "2013" = 2013,
-                              "2012" = 2012,
-                              "2011" = 2011,
-                              "2010" = 2010,
-                              "2009" = 2009,
-                              "2008" = 2008),
-                  selected = "2019"),
-      selectInput("selectX", 
-                  strong("Choose x variable"), 
-                  choices = c("Temperature" = "temperature", 
-                              "Dew Point"   = "dew.point", 
-                              "Humidity"    = "humidity", 
-                              "Pressure"    = "pressure",
-                              "Wind Speed Mean"    = "wind.speed.mean",
-                              "Wind Bearing Mean" = "wind.bearing.mean",
-                              "Time" = "ds"),
-                  selected = "temperature"),
-      selectInput("selectY", 
-                  strong("Choose y variable"), 
-                  choices = c("Temperature" = "temperature", 
-                              "Dew Point"   = "dew.point", 
-                              "Humidity"    = "humidity", 
-                              "Pressure"    = "pressure",
-                              "Wind Speed Mean"   = "wind.speed.mean",
-                              "Wind Bearing Mean" = "wind.bearing.mean"),
-                  selected = "dew.point")
+  navbarPage("Cambridge Weather",
+    tabPanel("Plot",
+      sidebarLayout(
+        sidebarPanel(
+          helpText("Explore ", tags$a(href="https://www.cl.cam.ac.uk/research/dtg/weather/", "ComLab"),  "weather measurements:"),
+          selectInput("year", 
+                      strong("Choose year"), 
+                      choices = c("2019" = 2019,
+                                  "2018" = 2018,
+                                  "2017" = 2017,
+                                  "2016" = 2016,
+                                  "2015" = 2015,
+                                  "2014" = 2014,
+                                  "2013" = 2013,
+                                  "2012" = 2012,
+                                  "2011" = 2011,
+                                  "2010" = 2010,
+                                  "2009" = 2009,
+                                  "2008" = 2008),
+                      selected = "2019"),
+          selectInput("selectX", 
+                      strong("Choose x variable"), 
+                      choices = c("Temperature" = "temperature", 
+                                  "Dew Point"   = "dew.point", 
+                                  "Humidity"    = "humidity", 
+                                  "Pressure"    = "pressure",
+                                  "Wind Speed Mean"    = "wind.speed.mean",
+                                  "Wind Bearing Mean" = "wind.bearing.mean",
+                                  "Time" = "ds"),
+                      selected = "temperature"),
+          selectInput("selectY", 
+                      strong("Choose y variable"), 
+                      choices = c("Temperature" = "temperature", 
+                                  "Dew Point"   = "dew.point", 
+                                  "Humidity"    = "humidity", 
+                                  "Pressure"    = "pressure",
+                                  "Wind Speed Mean"   = "wind.speed.mean",
+                                  "Wind Bearing Mean" = "wind.bearing.mean"),
+                      selected = "dew.point")
+        ),
+    
+        mainPanel(
+          plotOutput("plot"),
+          textOutput("selected_x")
+        )
+      )
     ),
-
-    mainPanel(
-      plotOutput("plot"),
-      textOutput("selected_x")
+    tabPanel("About",
+             includeMarkdown("README.md")
     )
 
 )))
